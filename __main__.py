80dfdbd71dcba0ec1060e4407558c791903b142b
Dylan <58540466+DylanVicharra@users.noreply.github.com>
2021-05-04 22:21:18 -0300

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,43 +1,133 @@
 from bot import Bot
 from time import sleep
+from datetime import date
 import elementos_web as ew
-from compra import seleccion_producto, transpaso_operador, completar_compra
+import pandas as pd
+import openpyxl as op
+import manejo_datos as ld
+import compra as compra
 
 
-def verificacion_datos(lista_datos):
-    if lista_datos == []:
-        print('El archivo no contiene datos')
-        exit(1)
-    else: 
-        return lista_datos
-
-
-def main():      
+def bot_compra(datos_iphone, operador, apple_id, cantidad):      
     # inicio el bot
-
     bot = Bot(ew.url_apple)
 
-    # Lista de datos que se utilizara
-    datos_domicilio = verificacion_datos(bot.leer_texto('datos_domicilio'))
-    datos_tarjeta = verificacion_datos(bot.leer_texto('datos_tarjeta'))
-    datos_iphone = verificacion_datos(bot.leer_texto('iphone'))
-    operador = datos_iphone[4]
+    # datos que se utilizara
     nro_operador = '7868639220'
+    cod_postal = '33178'
 
     # Usa diferentes botones la pagina si el celular es desbloqueado  
-     
-    if operador == 'unlocked':
-        seleccion_producto(bot, datos_iphone[0], datos_iphone[1], datos_iphone[2], datos_iphone[3], datos_iphone[4])
-    else:
-        seleccion_producto(bot, datos_iphone[0], datos_iphone[1], datos_iphone[2], datos_iphone[3], datos_iphone[4])
-        transpaso_operador(bot, nro_operador, datos_domicilio[4])
+    compra.seleccion_producto(bot, datos_iphone[0], datos_iphone[1], datos_iphone[2], datos_iphone[3], operador)
+    # Hace la verificacion del operador (solo si este no es unlocked)
+    if operador != 'unlocked':
+        compra.transpaso_operador(bot, nro_operador, cod_postal)
+    # Realiza en bolsa la seleccion de cantidad del producto 
+    compra.checkout(bot, cantidad, operador)
+    # Realiza el logueo del usuario
+    compra.logueo_appleid(bot, apple_id[0], apple_id[1])
+    # Realiza los siguientes, que son seleccion de envio y seleccion de tarjeta
+    compra.terminar_compra(bot) 
+    # Guarda en el bot la orden de compra, caso contrario termina
+    compra.obtener_orden(bot)
+
+    bot.finalizar()
+    return bot.get_estado(), bot.get_orden()
+
+def lectura_excel():
+    try: 
+        print("         ---------- BOT APPLE -----------                ")
+        print("SE NECESITA QUE SE INGRESE EL NOMBRE DE UN ARCHIVO EXCEL Y EL NOMBRE DE UNA HOJA DEL MISMO ARCHIVO ")
+        print("Se usara como archivo predeterminado el BOT.xlsx, Hoja: Compras")
+        # nombre del archivo excel a leer
+        archivo_excel = 'BOT'
+        hoja_calculo = 'Compras'  
+        # Elimino los espacios en blanco que tengan  
+        archivo_excel = archivo_excel.strip()
+        hoja_calculo = hoja_calculo.strip()
+        # Revisa si existe el archivo
+        ld.existe_archivo(archivo_excel)
+        # Abre el archivo con pandas para mejor manejo de los datos
+        excel_celulares = pd.read_excel(f'{archivo_excel}.xlsx', sheet_name=hoja_calculo, engine='openpyxl')
+    except:
+        print("HAY UN ERROR EN EL NOMBRE DE LA HOJA DE CALCULO QUE SE INGRESO")
+        exit(1)
     
-    completar_compra(bot, datos_domicilio, datos_tarjeta)
+    # Limpio la lista de datos vacios
+    excel_celulares = ld.limpieza_datos(excel_celulares)
+    
+    # Abre archivo a modificar 
+    excel_a_modificar = op.load_workbook(f'{archivo_excel}.xlsx')
+    
+    # Recorre cada fila del excel
+    for i in excel_celulares.index:
+        modelo = str(excel_celulares.loc[i,"MODELO"])
+        apple_id = [str(excel_celulares.loc[i,"USER"]),str(excel_celulares.loc[i,"PASSWORD"])]
+        operador = str(excel_celulares.loc[i,"OPERADOR"]).lower()
+        cantidad = int(excel_celulares.loc[i,"CANTIDAD"])
 
-    sleep(10)
+        datos_iphone = ld.verificacion_modelo(modelo)
 
-    del bot
+        print(f'SE ESTA COMPRANDO EL {modelo}')
+        # Realiza la cantidad de veces que dice la variable cantidad (en este caso todos son verizon)     
+        if operador != 'unlocked':   
+            for rep in range(cantidad):
+                try:
+                    print(f'nrº{rep+1} de {cantidad}')
+
+                    estado, orden = bot_compra(datos_iphone, operador, apple_id, cantidad)
+                
+                    if estado == 'Completado':
+                        # Escribo el estado
+                        ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Finalizado")
+                        # Escribo el link de orden
+                        ld.escribir_hipervinculo_excel(excel_a_modificar, hoja_calculo, i+2, 2, orden)
+                        # Escribo la fecha
+                        ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 1, str(date.today()))
+                        print("---------------------------------------------------------------------------------------------------------")
+                        sleep(10)
+                    elif estado == 'Fallido':
+                        # Escribo el estado
+                        ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Pendiente")
+                        print(f'HA FALLADO LA COMPRA DEL {modelo}') 
+                        print("---------------------------------------------------------------------------------------------------------")
+
+                except:
+                    # Escribo el estado
+                    ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Pendiente")
+
+                    print(f'HA FALLADO LA COMPRA DEL {modelo}')
+                    print("---------------------------------------------------------------------------------------------------------")
+
+        elif operador == 'unlocked':
+            try:
+                estado, orden = bot_compra(datos_iphone, operador, apple_id, cantidad)
+                
+                if estado == 'Completado':
+                    # Escribo el estado
+                    ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Finalizado")
+                    # Escribo el link de orden
+                    ld.escribir_hipervinculo_excel(excel_a_modificar, hoja_calculo, i+2, 2, orden)
+                    # Escribo la fecha
+                    ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 1, str(date.today()))
+                    print("---------------------------------------------------------------------------------------------------------")
+                    sleep(10)
+                elif estado == 'Fallido':
+                    # Escribo el estado
+                    ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Pendiente")
+
+                    print(f'HA FALLADO LA COMPRA DEL {modelo}') 
+                    print("---------------------------------------------------------------------------------------------------------")
+            except:
+                # Escribo el estado
+                ld.escribir_celda_excel(excel_a_modificar, hoja_calculo, i+2, 8, "Pendiente")
         
+                print(f'HA FALLADO LA COMPRA DEL {modelo}')
+                print("---------------------------------------------------------------------------------------------------------")
+
+    # Se guarda el archivo en cuestion
+    excel_a_modificar.save(f'{archivo_excel}.xlsx')
+
+    print("FINALIZANDO PROGRAMA")
 
 if __name__ == "__main__":
-    main()
+    lectura_excel()
--- bot.py
+++ bot.py
@@ -1,17 +1,26 @@
+import os.path as path
 from sys import platform
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException, WebDriverException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
 
 
 class Bot: 
     def __init__(self, url):
         os_confirm = self.ver_os()
-        self.driver = webdriver.Chrome(executable_path=f"{os_confirm}")
+        options = self.opciones()
+        self.driver = webdriver.Chrome(executable_path=f"{os_confirm}", chrome_options=options)
         self.driver.get(url)
+        self.estado = 'Espera'
+        self.link_orden = None
+
+    def get_driver(self):
+        driver = self.driver
+        return driver
 
     def ver_os(self):
         if platform == 'win32':
@@ -23,11 +32,17 @@ class Bot:
 
     def cambiar_url(self,nueva_url):
         self.driver.get(nueva_url)
+    
+    # Solo espera que el elemento este cargado en DOM
+    def elemento_cargado(self, tiempo_espera, elemento):
+        # Espera que cargue un pagina y contenedores
+        try: 
+            elemento = ec.presence_of_element_located((By.XPATH,elemento))
+            WebDriverWait(self.driver,tiempo_espera).until(elemento)      
+        except (TimeoutException):
+            print("TARDA EN CARGAR LA PAGINA")
 
-    def url_actual(self):
-        url_actual = str(self.driver.current_url)
-        return url_actual
-
+    # Selecciona al elemento que este cargado y visible 
     def esperar_elemento(self, tiempo_espera, elemento):
         espera_seleccion = False 
         while not espera_seleccion:
@@ -37,40 +52,36 @@ class Bot:
             except (ElementNotInteractableException, NoSuchElementException):
                 wait = WebDriverWait(self.driver,tiempo_espera)
                 wait.until(ec.visibility_of_element_located((By.XPATH, elemento)))
-            except (TimeoutException):
-                print("Posiblemente el elemento no se encuentra en la pagina")
-                exit(1)
+               
 
+    # Selecciona al elemento que solo este presente en el DOM 
     def encontrar_elemento(self, tiempo_espera, elemento):
         espera_seleccion = False 
         while not espera_seleccion:
             try: 
-                elemento_cargado = self.driver.find_element_by_xpath(elemento)
-                return elemento_cargado
-            except (ElementNotInteractableException, NoSuchElementException):
+                elemento_encontrado = self.driver.find_element_by_xpath(elemento)
+                return elemento_encontrado
+            except (NoSuchElementException):
                 wait = WebDriverWait(self.driver,tiempo_espera)
-                wait.until(ec.visibility_of_element_located((By.XPATH, elemento)))
-    
-    def leer_texto(self, archivo):
-        lista_datos = []
-        if archivo == 'datos_domicilio' or archivo == 'datos_tarjeta' or archivo == 'iphone':
-            lectura = open(f'{archivo}.txt','r')
-            for linea in lectura:
-                lista_datos.append(linea.strip('\n'))
-            lectura.close()
-            return lista_datos
-        else: 
-            print(f'El archivo {archivo}.text no admitido, renombrelo y vuelva intentar')
-            return lista_datos
+                wait.until(ec.presence_of_element_located((By.XPATH, elemento)))
 
-    def stock_disponible(self, tiempo_espera, elemento):
-        # Reviso en el recuadro de informacion si hay stock
-        stock = self.encontrar_elemento(tiempo_espera, elemento)
-        if (str(stock.text) == 'In Stock'):
-            return True
-        else: 
-            return False 
+    def finalizar(self):
+        self.driver.quit()
 
+    def get_estado(self):
+        return self.estado
 
+    def get_orden(self):
+        return self.link_orden
 
+    def opciones(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--log-level=3")
+        return chrome_options
 
+    def set_estado(self, nuevo_estado):
+        self.estado = nuevo_estado
+
+    def set_orden(self, nuevo_orden):
+        self.link_orden = nuevo_orden
