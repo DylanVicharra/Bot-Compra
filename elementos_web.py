b3c243702a12f08bfa9c692d86eb00ed1f20ae12
Dylan Vicharra <dylan.v9720@gmail.com>
2022-01-24 13:50:53 -0300

    Update elementos_web.py

--- elementos_web.py
+++ elementos_web.py
@@ -23,13 +23,13 @@ btn_activation_carrier_now = '//button[@type="submit"][@class="rf-carrier-fork-o
 
 ''' Pagina de validadcion del operador  '''
 # botones y textos para verificacion del operador
-contenedor_elementos_operador = '//div[@class="page-body"]'
-text_nr_operador = '//input[@type="tel"][@name="wirelessNumber"][@class="form-textbox-input"]'
-text_cod_postal_operador = '//input[@type="tel"][@name="postalcode"][@class="form-textbox-input"]'
-btn_siguiente_operador = '//button[@type="submit"][@class="button"]'
+contenedor_elementos_operador = '//div[@id="primary"]'
+text_nr_operador = '//input[@id="ctn"][@name="ctn"]'
+text_cod_postal_operador = '//input[@id="postalcode"][@name="postalcode"]'
+btn_siguiente_operador = '//button[@id="preauth-button-next"]'
 
 # botones en aviso del operador y descripcion del dispositivo
-btn_add_bag_2 = '//button[@type="submit"][@class="button"]'
+btn_add_bag_2 = '//a[@id="preauth-button-next"]'
 
 
 
